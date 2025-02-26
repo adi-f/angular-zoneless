c5472a5425564f088f8ea621910519fc2e15e6e9
Adrian Fahrer (u123603) <adrian.fahrer@mobi.ch>
2025-02-26 12:08:43 +0100

    moving/renaming

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Very minimalistic **zoneless** Angular app for demonstration purposes-
 
 ## Development
- * serve: `npm run start`
+ * serve: `npm run start` (go to http://localhost:4200/)
  * lint: `npm run lint`
  * format (prettier): `npm run format`
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { SimpleIncrementComponent } from './simple-increment/simple-increment.component';
-import { OnnchangeIncrementComponent } from './ngonchange-increment/onnchange-increment.component';
-import { ObservableIncrementComponent } from './observable-increment/observable-increment.component';
-import { SignalInputIncrementComponent } from './signal-input-increment/signal-input-increment.component';
-import { SignalAsyncIncrementComponent } from './signal-async-increment/signal-async-increment.component';
-import { AsyncIncrementComponent } from './async-increment/async-increment.component';
+import { SimpleIncrementComponent } from './01-simple-increment/simple-increment.component';
+import { OnnchangeIncrementComponent } from './02 ngonchange-increment/onnchange-increment.component';
+import { ObservableIncrementComponent } from './03-observable-increment/observable-increment.component';
+import { SignalInputIncrementComponent } from './04-signal-input-increment/signal-input-increment.component';
+import { SignalAsyncIncrementComponent } from './05-signal-async-increment/signal-async-increment.component';
+import { AsyncIncrementComponent } from './06 async-increment/async-increment.component';
 
 @Component({
   selector: 'app-root',
